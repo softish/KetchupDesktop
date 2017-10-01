34414b3a5df079178e3ba8c139129702341d9fdc
softish <lazar.h.klincov@live.se>
2017-10-01 13:05:23 +0200

    Added basic GUI.

--- src/main/java/view/KetchupDesktopView.java
+++ src/main/java/view/KetchupDesktopView.java
@@ -1,7 +1,35 @@
 package view;
 
-import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 
-public class KetchupDesktopView extends GridPane {
+public class KetchupDesktopView extends BorderPane {
 
+    private Button resetButton;
+    private Button changeStateButton;
+
+    public KetchupDesktopView() {
+        initView();
+    }
+
+    private void initView() {
+        resetButton = new Button("Reset");
+        changeStateButton = new Button("Start");
+
+        Label label = new Label("00:00:00");
+        label.setStyle("-fx-font-size: 42px; -fx-font-weight: bold;");
+
+        FlowPane buttonsPane = new FlowPane();
+        buttonsPane.getChildren().add(resetButton);
+        buttonsPane.getChildren().add(changeStateButton);
+        buttonsPane.setHgap(10);
+        buttonsPane.setAlignment(Pos.CENTER);
+
+        this.setCenter(label);
+        this.setBottom(buttonsPane);
+    }
 }
