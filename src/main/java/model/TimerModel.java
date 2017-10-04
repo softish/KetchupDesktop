7aeb718e3c4e306b894a46356b9112efe85682da
softish <lazar.h.klincov@live.se>
2017-10-04 15:16:53 +0200

    Refactored TimerModelTask visibility to match tight coupling with TimerModel.

--- src/main/java/model/TimerModel.java
+++ src/main/java/model/TimerModel.java
@@ -74,4 +74,41 @@ public class TimerModel implements Subject {
             observer.update(timerEvent);
         }
     }
+
+    private class TimerModelTask extends Thread {
+
+        private long timeTarget;
+        private TimerModel timerModel;
+        private boolean end;
+
+        public TimerModelTask(long timeTarget, TimerModel timerModel) {
+            this.timeTarget = timeTarget;
+            this.timerModel = timerModel;
+        }
+
+        @Override
+        public void run() {
+            while (timeTarget > 0) {
+                try {
+                    if(end) {
+                        break;
+                    }
+
+                    timeTarget -= 1000;
+                    timerModel.setTimeLeft(timeTarget);
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(timeTarget == 0) {
+                timerModel.timeout();
+            }
+        }
+
+        public void end() {
+            end = true;
+        }
+    }
 }
