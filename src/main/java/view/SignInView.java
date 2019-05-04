ccd7976623fbac77601d1d5cd6c0b3b60a746e21
softish <lazar.h.klincov@live.se>
2019-05-04 20:36:18 +0200

    Deduplicate dialog code

--- src/main/java/view/SignInView.java
+++ src/main/java/view/SignInView.java
@@ -53,57 +53,21 @@ public class SignInView extends BorderPane {
     }
 
     public Optional<Pair<String, String>> showLoginDialog() {
-        Dialog<Pair<String, String>> dialog = new Dialog<>();
-        dialog.setTitle("Sign In Dialog");
-        dialog.setHeaderText(null);
-        dialog.getDialogPane().setStyle("-fx-background: #1E1E1E; -fx-fill: #DCDCDC;");
-
-        ButtonType loginButtonType = new ButtonType("Login", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
-
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20, 150, 10, 10));
-
-        TextField username = new TextField();
-        username.setPromptText("Username");
-        PasswordField password = new PasswordField();
-        password.setPromptText("Password");
-
-        grid.add(new Label("Username:"), 0, 0);
-        grid.add(username, 1, 0);
-        grid.add(new Label("Password:"), 0, 1);
-        grid.add(password, 1, 1);
-
-        dialog.getDialogPane().setContent(grid);
-
-        Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
-        loginButton.setDisable(true);
-
-        username.textProperty().addListener((observable, oldValue, newValue) -> loginButton.setDisable(newValue.trim().isEmpty() || password.getText().trim().isEmpty()));
-        password.textProperty().addListener((observable, oldValue, newValue) -> loginButton.setDisable(newValue.trim().isEmpty() || username.getText().trim().isEmpty()));
-
-        username.requestFocus();
-
-        dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == loginButtonType) {
-                return new Pair<>(username.getText(), password.getText());
-            }
-            return null;
-        });
-
-        return dialog.showAndWait();
+        return createDialogWithTwoField("Sign In Dialog", "Login").showAndWait();
     }
 
     public Optional<Pair<String, String>> showRegisterDialog() {
+        return createDialogWithTwoField("Register Dialog", "Register").showAndWait();
+    }
+
+    private Dialog<Pair<String, String>> createDialogWithTwoField(String dialogTitle, String buttonText) {
         Dialog<Pair<String, String>> dialog = new Dialog<>();
-        dialog.setTitle("Register Dialog");
+        dialog.setTitle(dialogTitle);
         dialog.setHeaderText(null);
         dialog.getDialogPane().setStyle("-fx-background: #1E1E1E; -fx-fill: #DCDCDC;");
 
-        ButtonType registerButtonType = new ButtonType("Register", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(registerButtonType, ButtonType.CANCEL);
+        ButtonType confirmationButton = new ButtonType(buttonText, ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(confirmationButton, ButtonType.CANCEL);
 
         GridPane grid = new GridPane();
         grid.setHgap(10);
@@ -122,20 +86,21 @@ public class SignInView extends BorderPane {
 
         dialog.getDialogPane().setContent(grid);
 
-        Node registerButtonForDialog = dialog.getDialogPane().lookupButton(registerButtonType);
-        registerButtonForDialog.setDisable(true);
+        Node confirmationButtonInPane = dialog.getDialogPane().lookupButton(confirmationButton);
+        confirmationButtonInPane.setDisable(true);
 
-        username.textProperty().addListener((observable, oldValue, newValue) -> registerButtonForDialog.setDisable(newValue.trim().isEmpty() || password.getText().trim().isEmpty()));
-        password.textProperty().addListener((observable, oldValue, newValue) -> registerButtonForDialog.setDisable(newValue.trim().isEmpty() || username.getText().trim().isEmpty()));
+        username.textProperty().addListener((observable, oldValue, newValue) -> confirmationButtonInPane.setDisable(newValue.trim().isEmpty() || password.getText().trim().isEmpty()));
+        password.textProperty().addListener((observable, oldValue, newValue) -> confirmationButtonInPane.setDisable(newValue.trim().isEmpty() || username.getText().trim().isEmpty()));
+
         username.requestFocus();
 
         dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == registerButtonType) {
+            if (dialogButton == confirmationButton) {
                 return new Pair<>(username.getText(), password.getText());
             }
             return null;
         });
 
-        return dialog.showAndWait();
+        return dialog;
     }
 }
